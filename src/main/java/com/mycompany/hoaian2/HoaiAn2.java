ff6c94d22daeda194acc5d8b5fb7892dac2c5eb7
Vo Quang Vinh <vqvinh243@gmail.com>
2018-08-03 00:54:04 +0700

    init intellij form

---
+++ src/main/java/com/mycompany/hoaian2/HoaiAn2.java
@@ -0,0 +1,15 @@
+package com.mycompany.hoaian2;
+
+import javax.swing.*;
+
+public class HoaiAn2 {
+    private JTabbedPane tabbedPane1;
+    private JPanel panel1;
+    private JComboBox inputSelectorCbx;
+    private JComboBox buyLevelCbx;
+    private JComboBox saleLevelCbx;
+    private JList productList;
+    private JTable orderTbl;
+    private JButton button2;
+    private JButton button1;
+}
